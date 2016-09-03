4653e9d6fbec39ff8b1ce15ba989829ee2970276
= <piper.bied@gmail.com>
2016-09-03 16:13:28 +0300

    not real data

--- recommendation/criticism.py
+++ recommendation/criticism.py
@@ -1,1 +1,9 @@
-critcs = {}
+critcs = {
+'Lusa Rose':{'Lady in the Water':2.5,'Snakes on a Plane':3.5,'Just My Listenet':3.0,'Superman Returns':3.5,'You, Me and Dupree':2.5,'The Night Listener':3.0},
+'Gene Seymouy':{'Lady in the Water':3.0,'Snakes on a Plane':3.5,'Just My Listenet':1.5,'Superman Returns':5.0,'The Night Listener':3.0,'You, Me and Dupree':3.5},
+'Michel Philips':{'Lady in the Water':2.5,'Snakes on a Plane':3.0,'Superman Returns':3.5,'The Night Listener':4.0},
+'Claudia Puig':{'Snakes on a Plane':3.5,'Just My Listenet':3.0,'The Night Listener':4.5,'Superman Returns':4.0,'You, Me and Dupree':2.5},
+'Mick Lasalle':{'Lady in the Water':3.0,'Snakes on a Plane':4.0,'Just My Listenet':2.0,'Superman Returns':3.0,'The Night Listener':3.0,'You, Me and Dupree':2.0},
+'Jack Matthews':{'Lady in the Water':3.0,'Snakes on a Plane':4.0,'The Night Listener':3.0,'Superman Returns':5.0,'You, Me and Dupree':3.5},
+'Toby':{'Snakes on a Plane':4.5,'You, Me and Dupree':1.0,'Superman Returns':4.0}
+}

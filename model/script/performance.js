49beeabd3bc1a0ca3488dbf3fd1376c33dbf3d61
FORSEN-ROCK <piper.bied@gmail.com>
2017-03-16 23:12:51 +0300

    create performens

---
+++ model/script/performance.js
@@ -0,0 +1,21 @@
+function addDataTable(parentElm,dataList) { 
+    //parentElement - Table
+    //dataList = [data1,data2,...,dataN]
+    //data = {'atribut1':<value1>,'atribut2':<value2>,...,'atributN':<valueN>}
+    if((parentElm != undefined) && (dataList != [])){
+        var iframe = document.createDocumentFragment();
+        for( var i = 0; i < dataList.length; i++) {
+            var tableRow = document.createElement('tr');
+            for( var key in dataList[i]) {
+                var tableh = document.createElement('th');
+                if(dataList[i][key] != undefined)
+                    tableh.insertAdjacentHTML('beforeEnd',dataList[i][key]);
+                else
+                    tableh.insertAdjacentHTML('beforeEnd','');
+                tableRow.appendChild(tableh);
+            }
+            iframe.appendChild(tableRow);
+        }
+        parentElm.appendChild(iframe);
+    }
+};
